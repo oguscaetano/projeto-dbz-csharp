a88e83a75f922d0a4b63b1416c0f2cc2c91bebe1
Gus - Windows <gustavo.caetano13@gmail.com>
2025-04-02 19:58:09 -0300

    adiciona o metodo GET

--- Controllers/PersonagemController.cs
+++ Controllers/PersonagemController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Xablau.Data;
+using Xablau.Models;
 
 namespace Xablau.Controllers
 {
@@ -12,6 +14,27 @@ namespace Xablau.Controllers
         public PersonagemController(AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPersonagem(Personagem personagem)
+        {
+            if (personagem == null) {
+                return BadRequest("Dados inválidos!");
+            }
+
+            _appDbContext.XablauDB.Add(personagem);
+            await _appDbContext.SaveChangesAsync();
+
+            return StatusCode(201, personagem);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult <IEnumerable<Personagem>>> GetPersonagem()
+        {
+            var personagens = await _appDbContext.XablauDB.ToListAsync();
+
+            return Ok(personagens);
         }
     }
 }
